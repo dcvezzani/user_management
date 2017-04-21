dea1152115c4a68cb678eee5aac3b4ef483311f9
David Vezzani <david_vezzani@yahoo.com>
2017-04-21 06:42:03 -0700

    init

---
+++ server.js
@@ -0,0 +1,12 @@
+console.log('May Node be with you')
+
+const express = require('express');
+const app = express();
+
+app.listen(3000, function() {
+  console.log('listening on 3000')
+})
+
+app.get('/', function(req, res) {
+  res.send('Hello World')
+})
